7195ba913b31a68faf91a806d7bd13e85c4120ca
alex <olegthander@yander.ru>
2021-03-23 22:41:56 +0300

    DUNDEV-11 заменил везде статическое использование цвета фона на использование переменной

--- app/src/main/java/com/dungeonmaster/LoadingScreen.java
+++ app/src/main/java/com/dungeonmaster/LoadingScreen.java
@@ -3,7 +3,11 @@ package com.dungeonmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.dungeonmaster.menu.DataSynchronizer;
+import com.dungeonmaster.menu.MainMenu;
 
 public class LoadingScreen extends Activity {
 
@@ -11,5 +15,9 @@ public class LoadingScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        // Запускую отдельный поток на загрузку данных с сервера.
+        Thread synchronizer = new Thread(new DataSynchronizer(this));
+        synchronizer.start();
     }
 }
---
+++ app/src/main/java/com/dungeonmaster/menu/DataSynchronizer.java
@@ -0,0 +1,30 @@
+package com.dungeonmaster.menu;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.dungeonmaster.LoadingScreen;
+
+public class DataSynchronizer implements Runnable {
+
+    private Activity loadingScreen;
+
+    public DataSynchronizer(LoadingScreen loadingScreen) {
+        this.loadingScreen = loadingScreen;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Пока просто останавливаем чтобы увидеть начальный экран с лого
+            // TODO: реализовать подключение к базе, синхронизацию аккаунта в момент загрузки экрана
+            Thread loadingThread = new Thread();
+            loadingThread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent mainMenu = new Intent(loadingScreen, MainMenu.class);
+        loadingScreen.startActivity(mainMenu);
+    }
+
+}
