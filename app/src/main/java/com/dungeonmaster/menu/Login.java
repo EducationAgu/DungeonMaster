43be7e24fa90c642bce45d31a3654a120ae072ac
Hathawway <mkim95165@gmail.com>
2021-03-30 12:23:49 +0400

    migrated to android x

---
+++ app/src/main/java/com/dungeonmaster/menu/Login.java
@@ -0,0 +1,16 @@
+package com.dungeonmaster.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import com.dungeonmaster.R;
+
+
+public class Login extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
