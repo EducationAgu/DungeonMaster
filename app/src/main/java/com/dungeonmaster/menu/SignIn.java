8391e00d8f5921b21bbda4db54de208c3822f8fa
Maryana-Garetz <mrnaafna@gmail.com>
2021-04-20 15:01:53 +0400

    добавлена валидация при регистрации и логине

--- app/src/main/java/com/dungeonmaster/menu/SignIn.java
+++ app/src/main/java/com/dungeonmaster/menu/SignIn.java
@@ -3,6 +3,7 @@ package com.dungeonmaster.menu;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -10,15 +11,18 @@ import android.widget.Toast;
 import com.dungeonmaster.LoadingScreen;
 import com.dungeonmaster.R;
 import com.serverconnection.Server;
-import com.serverconnection.URLs;
 import com.serverconnection.model.User;
 
-import org.springframework.http.HttpMethod;
+import java.io.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignIn extends Activity {
 
     private EditText loginTextBox;
     private EditText passwordTextBox;
+
+    private UserDataChecker userDataChecker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +30,25 @@ public class SignIn extends Activity {
         setContentView(R.layout.signin_window);
         loginTextBox = findViewById(R.id.emailField);
         passwordTextBox = findViewById(R.id.passwordField);
+
+        userDataChecker = new UserDataChecker(this);
     }
 
     public void onClickLogin(View view) {
         User user = new User();
 
         String login = loginTextBox.getText().toString();
-        user.setLogin(login);
+        String password = passwordTextBox.getText().toString();
+        if (userDataChecker.isValidEmail(login) && userDataChecker.isValidPassword(password)) {
+            user.setLogin(login);
+            user.setPassword(password);
 
-        user.setPassword(passwordTextBox.getText().toString());
+            Server.login(user, this);
+            Intent loadingScreen = new Intent(this, LoadingScreen.class);
+            startActivity(loadingScreen);
+            overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+        }
 
-        Server.login(user, this);
-        Intent loadingScreen = new Intent(this, LoadingScreen.class);
-        startActivity(loadingScreen);
-        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
     }
 
     public void onClickOpenRegistration(View view){
@@ -47,4 +56,5 @@ public class SignIn extends Activity {
         startActivity(nextScreen);
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
     }
+
 }
