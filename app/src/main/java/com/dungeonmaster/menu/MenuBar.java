8baa590924ba8c7350c625326a000c5443a462c6
Буков Александр Викторович (DigitalSpirit) <olegthander@yander.ru>
2021-04-22 17:26:52 +0300

    релиз 0.0.1

--- app/src/main/java/com/dungeonmaster/menu/MenuBar.java
+++ app/src/main/java/com/dungeonmaster/menu/MenuBar.java
@@ -1,6 +1,7 @@
 package com.dungeonmaster.menu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.dungeonmaster.R;
+import com.serverconnection.Server;
 
 public class MenuBar extends AppCompatActivity {
 
@@ -27,6 +29,12 @@ public class MenuBar extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.menu1:
+                Server.logout(this);
+                Intent signIn = new Intent(this, SignIn.class);
+                startActivity(signIn);
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
