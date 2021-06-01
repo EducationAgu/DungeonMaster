6abd3fe5587ec868a8d9384d462d6632918f5eeb
Буков Александр Викторович (DigitalSpirit) <olegthander@yander.ru>
2021-06-01 11:46:55 +0300

    characterdata

--- app/src/main/java/com/dungeonmaster/games/dnd/CreateCharacter.java
+++ app/src/main/java/com/dungeonmaster/games/dnd/CreateCharacter.java
@@ -1,8 +1,10 @@
 package com.dungeonmaster.games.dnd;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.dungeonmaster.R;
+import com.dungeonmaster.games.dnd.character.CharacterData;
 import com.dungeonmaster.menu.MainMenu;
 import com.menu.MenuBar;
 
@@ -13,6 +15,11 @@ public class CreateCharacter extends MenuBar {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_character);
     }
+
+    public void onClickOpenCharacterData(View view){
+        setNextScreen(CharacterData.class, TO_LEFT);
+    }
+
     @Override
     public void onBackPressed() {
         setNextScreen(DnDHelper.class, TO_LEFT);
